ab9e0f724c45734b1cf2d5e5c7a6fb15f6797f97
Tim Davis <binary.god@gmail.com>
2024-08-09 21:09:22 -0400

    Added samples

---
+++ Get-LAPSPassword.ps1
@@ -0,0 +1,23 @@
+Function Get-LAPSPassword() {
+    [cmdletbinding()]
+    param(
+        [String]$ComputerName,
+        [pscredential]$Credential
+    )
+    begin {
+
+    }
+    process {
+        # Password Attribute: ms-Mcs-AdmPwd
+        # LAPS Exipre Attribute: ms-Mcs-AdmPwdExpirationTime
+
+        $computer = Get-ADComputer -Identity $ComputerName -Server DC01.binarymethod.com -Credential $Credential -Properties ms-Mcs-AdmPwd, ms-Mcs-AdmPwdExpirationTime
+    
+        [ordered]@{
+            Computer  = $computer.Name
+            Password  = $computer."ms-Mcs-AdmPwd"
+            ExpiresOn = [DateTime]::FromFileTimeUtc($computer."ms-Mcs-AdmPwdExpirationTime")
+        }
+        # $computer
+    }
+}
