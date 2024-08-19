084920b7994d993bf8afe9cdc76cf0124c972dfa
Tim Davis <binary.god@gmail.com>
2024-08-19 07:19:23 -0400

    Update ParamSplat.ps1

--- Examples/Parameter Splat/ParamSplat.ps1
+++ Examples/Parameter Splat/ParamSplat.ps1
@@ -4,7 +4,7 @@
 # Parameter Splat allows you to define the parameters in a hashtable and then 'splat' those to a cmdlet all
 # at once
 
-# Word normally a cmdlet like so
+# Would normally call a cmdlet like so
 .\Show-ParamSplat.ps1 -Name "Tim" -Age "44" -Force
 
 # Using param splat we can do this
@@ -21,8 +21,6 @@ $params = @{
 
 # Why would you use this?
 # I've used it for when I need to call a cmdlet but may not always call it with the same
-# parameters, for example when if some condition is true I may run cmdletA with an additional
+# parameters, for example when some condition is true I may run cmdletA with an additional
 # parameter, and instead of doing if statements for all possible combinations i would create
 # a hashtable of parameters.
-
-# See Get-TSxADUser.ps1
